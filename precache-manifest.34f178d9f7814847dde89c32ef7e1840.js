e620f837a867b36bfb509bd4b0dd0f9b648c9442
suzmar94 <suzmar94@gmail.com>
2020-09-24 00:00:59 +0200

    Updates

---
+++ precache-manifest.34f178d9f7814847dde89c32ef7e1840.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e03d52759d995ef9a5908a3183cacfba",
+    "url": "/final-project/index.html"
+  },
+  {
+    "revision": "79e9b1d943a7fb5aa981",
+    "url": "/final-project/static/css/main.582c1948.chunk.css"
+  },
+  {
+    "revision": "bddfdc4cebe5679dc252",
+    "url": "/final-project/static/js/2.63ca7f0b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/final-project/static/js/2.63ca7f0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79e9b1d943a7fb5aa981",
+    "url": "/final-project/static/js/main.1abace5e.chunk.js"
+  },
+  {
+    "revision": "1d0a543f8ee339794feb",
+    "url": "/final-project/static/js/runtime-main.44594cc3.js"
+  }
+]);
